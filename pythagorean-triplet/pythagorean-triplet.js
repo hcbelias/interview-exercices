7819f3ae4634de85330a78af06b76a2a3b9bb1ab
Henrique Elias <henrique.elias@hotmart.com>
2022-01-26 16:49:21 -0300

    creating test case one

--- pythagorean-triplet/pythagorean-triplet.js
+++ pythagorean-triplet/pythagorean-triplet.js
@@ -1,10 +1,26 @@
 export default function checkTriplet(arr, n) {
   const squareMapperResult = {}
+
   for (let i = 0; i < n; i++) {
     const squareValue = Math.pow(arr[i], 2)
     if (typeof squareMapperResult[squareValue] === 'undefined') {
       squareMapperResult[squareValue] = [i]
+    } else {
+      squareMapperResult.push(squareValue)
     }
-    squareMapperResult.push(squareValue)
   }
+
+  for (let i = 0; i < n - 2; i++) {
+    const squareValueOne = Math.pow(arr[i], 2)
+    for (let j = i + 1; j < n; j++) {
+      const squareValueTwo = Math.pow(arr[j], 2)
+      const squareSum = squareValueOne + squareValueTwo
+
+      if (typeof squareMapperResult[squareSum] !== 'undefined') {
+        return true
+      }
+    }
+  }
+
+  return false
 }
--- pythagorean-triplet/pythagorean-triplet.test.js
+++ pythagorean-triplet/pythagorean-triplet.test.js
@@ -1,7 +1,12 @@
 import checkTriplet from './pythagorean-triplet'
 
+import TestCaseOne from './test-cases/test-case-1';
+
 describe('pythagorean-triplet', () => {
-  xit('[3, 2, 4, 6, 5] has a pythagorean triplet', () => {
+  it('[3, 2, 4, 6, 5] has a pythagorean triplet', () => {
     expect(checkTriplet([3, 2, 4, 6, 5], 5)).toBe(true)
+  })
+  it('TestCaseOne has a pythagorean triplet', () => {
+    expect(checkTriplet(TestCaseOne.arr, TestCaseOne.arr)).toBe(true)
   })
 })
