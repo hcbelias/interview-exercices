e3e3075d138ee6c920f914aa50cde3d76ee29a00
Henrique Elias <henrique.elias@hotmart.com>
2022-01-26 15:54:42 -0300

    using yarn/config babel

--- pythagorean-triplet/pythagorean-triplet.test.js
+++ pythagorean-triplet/pythagorean-triplet.test.js
@@ -1,7 +1,7 @@
 import checkTriplet from './pythagorean-triplet'
 
 describe('pythagorean-triplet', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(checkTriplet(1, 2)).toBe(3)
+  test('[3, 2, 4, 6, 5] has a pythagorean triplet', () => {
+    expect(checkTriplet([3, 2, 4, 6, 5], 5)).toBe(true)
   })
 })
