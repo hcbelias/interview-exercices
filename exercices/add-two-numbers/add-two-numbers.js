481a1b4968fa64c786dfbda156fa994970a4af9a
Henrique Elias <henrique.elias@hotmart.com>
2022-01-28 15:57:41 -0300

    creating add-two-number structure test

--- exercices/add-two-numbers/add-two-numbers.js
+++ exercices/add-two-numbers/add-two-numbers.js
@@ -1,8 +1,21 @@
+export function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val
+  this.next = next === undefined ? null : next
+}
+
+
+export function buildLinkedList(array) {
+
+
+}
+
+
+
 /**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
+ * @param {ListNode} l1 First number to be added in ListNode structure
+ * @param {ListNode} l2 Second number to be added in ListNode structure
+ * @return {ListNode} Result from the sum between two number in ListNode structure
  */
-export default function addTwoNumbers(arr, n) {
+export default function addTwoNumbers(l1, l2) {
   
 }
--- exercices/add-two-numbers/add-two-numbers.test.js
+++ exercices/add-two-numbers/add-two-numbers.test.js
@@ -1,4 +1,32 @@
-import addTwoNumbers from './add-two-numbers'
+import addTwoNumbers, { ListNode, buildLinkedList } from './add-two-numbers'
 
 describe('add-two-numbers', () => {
+  describe('ListNode function', () => {
+  })
+  describe('buildLinkedList function', () => {
+    
+  })
+  describe('add-two-numbers function', () => {
+    it('Should_Return [7,0,8]_When_Passing [2,4,3] as l1 and [5,6,4] as l2', () => {
+      const l1 = [2, 4, 3]
+      const l2 = [5, 6, 4]
+      const expected = [7, 0, 8]
+      // need to review due to ref comparison
+      expect(addTwoNumbers(l1, l2)).toBe(expected)
+    })
+    it('Should_Return [0]_When_Passing [0] as l1 and [0] as l2', () => {
+      const l1 = [0]
+      const l2 = [0]
+      const expected = [0]
+      // need to review due to ref comparison
+      expect(addTwoNumbers(l1, l2)).toBe(expected)
+    })
+    it('Should_Return [8,9,9,9,0,0,0,1]_When_Passing [9,9,9,9,9,9,9] as l1 and [9,9,9,9] as l2', () => {
+      const l1 = [9, 9, 9, 9, 9, 9, 9]
+      const l2 = [9, 9, 9, 9]
+      const expected = [8, 9, 9, 9, 0, 0, 0, 1]
+      // need to review due to ref comparison
+      expect(addTwoNumbers(l1, l2)).toBe(expected)
+    })
+  })
 })
