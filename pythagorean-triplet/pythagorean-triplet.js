0aae058265a0f57a4c1d2fe3ea97e4202c61adac
Henrique Elias <henrique.elias@hotmart.com>
2022-01-28 11:01:15 -0300

    running tests

--- pythagorean-triplet/pythagorean-triplet.js
+++ pythagorean-triplet/pythagorean-triplet.js
@@ -1,24 +1,42 @@
+
 export default function checkTriplet(arr, n) {
-  const squareMapperResult = {}
-
-  for (let i = 0; i < n; i++) {
-    const squareValue = Math.pow(arr[i], 2)
-    if (typeof squareMapperResult[squareValue] === 'undefined') {
-      squareMapperResult[squareValue] = [i]
-    } else {
-      squareMapperResult.push(squareValue)
-    }
+  if (n < 3) {
+    return false
   }
+  // creating a sorted array with squared values
+  const squareResults = arr.map(item => Math.pow(item, 2)).sort((a, b) => a - b)
 
-  for (let i = 0; i < n - 2; i++) {
-    const squareValueOne = Math.pow(arr[i], 2)
-    for (let j = i + 1; j < n; j++) {
-      const squareValueTwo = Math.pow(arr[j], 2)
-      const squareSum = squareValueOne + squareValueTwo
+  // Considering a > b > c
+  let a = squareResults.length - 1 // a from pythagorean triplet formula, starting from last array element
+  const hypotenuseSquareValue = Math.pow(arr[a], 2)
+  /***************
+        [1, 4, 9, 16, 25, 36, 49, 64, 81]
+        ^              ^               ^
+    leftIndex      rightIndex          a
+  ***************/
 
-      if (typeof squareMapperResult[squareSum] !== 'undefined') {
-        return true
-      }
+  let leftIndex = 0 // c candidate from pythagorean triplet formula
+  let rightIndex = a - 1 // b candidade from pythagorean triplet formula
+
+  // we need to check while a is in third position in array
+  while (a > 1) {
+    const leftSideValue = Math.pow(arr[leftIndex], 2)
+    const rightSideValue = Math.pow(arr[rightIndex], 2)
+    const sideSquareSumValue = leftSideValue + rightSideValue
+    if (sideSquareSumValue < hypotenuseSquareValue) {
+      leftIndex++
+    } else if (sideSquareSumValue > hypotenuseSquareValue) {
+      rightIndex--
+    } else {
+      // matched conditions
+      return true
+    }
+
+    // Indexes matches without finding candidate, we should look for another hypotenuse value
+    if (leftIndex === rightIndex) {
+      a--
+      leftIndex = 0
+      rightIndex = a - 1
     }
   }
 
--- pythagorean-triplet/pythagorean-triplet.test.js
+++ pythagorean-triplet/pythagorean-triplet.test.js
@@ -1,12 +1,15 @@
 import checkTriplet from './pythagorean-triplet'
 
-import TestCaseOne from './test-cases/test-case-1';
+import TestCaseOne from './test-cases/test-case-1'
 
 describe('pythagorean-triplet', () => {
-  it('[3, 2, 4, 6, 5] has a pythagorean triplet', () => {
+  it('Should [3, 2, 4, 6, 5] expect to return true', () => {
     expect(checkTriplet([3, 2, 4, 6, 5], 5)).toBe(true)
   })
-  it('TestCaseOne has a pythagorean triplet', () => {
-    expect(checkTriplet(TestCaseOne.arr, TestCaseOne.arr)).toBe(true)
+  it('Should [3, 8, 5] expect to return false', () => {
+    expect(checkTriplet([3, 8, 5], 3)).toBe(false)
+  })
+  it.only('TestCaseOne has a pythagorean triplet', () => {
+    expect(checkTriplet(TestCaseOne.arr, TestCaseOne.n)).toBe(true)
   })
 })
