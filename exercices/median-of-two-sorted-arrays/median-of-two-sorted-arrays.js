5ddaa7ebe0e4a8b72db22db9e4d9778443718ca7
Henrique Elias <henrique.elias@hotmart.com>
2022-01-29 16:07:30 -0300

    adding comments

--- exercices/median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ exercices/median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -1,1 +1,7 @@
+/**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+ * @param {number[]} nums1 - first number array
+ * @param {number[]} nums2 - second number array
+ * @return {number} returns the media between the arrays
+ */
 export default function findMedianSortedArrays(nums1, nums2) {}
